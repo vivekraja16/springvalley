00d7d16b9fa8d2fd86ce85d816b8739d8aae47a1
vivekraja16 <vivek16@gmail.com>
2017-08-09 00:38:46 +0530

    Small fix in the popup page location

--- gallery.php
+++ gallery.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="box" id="lightgallery">
                 <?php for($i=1;$i<=24;$i++) { ?>
-				<div class="col-sm-3" data-src="img/Images/Spring<?php echo $i; ?>.JPG">
+				<div class="col-sm-3" data-src="Images/Spring<?php echo $i; ?>.JPG">
                   <a href="">
                     <img class="img-responsive img-border img-full" src="Images/Spring<?php echo $i;?>.JPG" alt="">
                   </a>
