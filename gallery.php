aa29c9b740a31f0204a7965e3e644f05693bed74
Gopi Raja <info@gopir13.com>
2017-08-16 20:20:39 +0530

    Title tag added

--- gallery.php
+++ gallery.php
@@ -5,9 +5,10 @@
         <div class="row">
             <div class="box" id="lightgallery">
                 <?php for($i=1;$i<=24;$i++) { ?>
-				<div class="col-sm-3" data-src="Images/Spring<?php echo $i; ?>.JPG">
+	              <div class="col-sm-3" data-src="Images/Spring<?php echo $i; ?>.JPG" data-sub-html="Title">
                   <a href="">
                     <img class="img-responsive img-border img-full" src="Images/Spring<?php echo $i;?>.JPG" alt="">
+                    <span>Title</span>
                   </a>
                 </div>
 				<?php } ?>
